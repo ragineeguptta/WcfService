f29511a35001d68443ab3764942f0133dd62dc3a
Raginee <raginee.gupta@hirdhav.com>
2022-07-04 18:23:28 +0530

    Add project files.

---
+++ HelloObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfService
+{
+    public class HelloObject
+    { 
+        [DataMember]
+        public bool happyHello { get; set; } = false;
+
+
+        [DataMember]
+        public string helloMessage { get; set; } = "Hello";
+
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(string value)
+        {
+            return "If your voice travels " + value + " feet, then the influence of your voice will cover " + Int32.Parse(value) * Int32.Parse(value) * 3.14 + " sq feet";
+        }
+
+
+        public string SayHello()
+        {
+            return "Hello, I am Raginee Gupta.";
+        }
+        public HelloObject GetModelObject(string id)
+        {
+            HelloObject helloObject = new HelloObject();
+            if (Int32.Parse(id) > 0)
+            {
+                helloObject.happyHello = true;
+                helloObject.helloMessage = "Great Start";
+            }
+            else
+            {
+                helloObject.happyHello = false;
+                helloObject.helloMessage = "Feeling very glum. ";
+            }
+            return helloObject;
+        }
+    }
+}
